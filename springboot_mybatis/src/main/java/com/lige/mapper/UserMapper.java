d1676b4d196781950f6f0234d971e87f85f8f7d5
Lisir <victor1150667182@outlook.com>
2020-10-03 17:10:37 +0800

    将java工程添加到版本库

---
+++ springboot_mybatis/src/main/java/com/lige/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.lige.mapper;
+
+import com.lige.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    public List<User> queryUserList();
+}
